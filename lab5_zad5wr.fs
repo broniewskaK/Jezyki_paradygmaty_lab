8b7b531a815db64ee6a680aa0ac2edad010c101d
broniewskaK <115491327+broniewskaK@users.noreply.github.com>
2024-12-20 16:40:00 +0100

    Add files via upload

---
+++ lab5_zad5wr.fs
@@ -0,0 +1,14 @@
+﻿
+// Funkcja rekurencyjna QuickSort
+let rec quickSortRecursive list =
+    match list with
+    | [] -> []
+    | pivot :: tail ->
+        let smaller = List.filter (fun x -> x <= pivot) tail
+        let greater = List.filter (fun x -> x > pivot) tail
+        (quickSortRecursive smaller) @ [pivot] @ (quickSortRecursive greater)
+
+let unsortedList = [89999; 6; 8; 10; 1; 87; 1]
+
+let sortedRecursive = quickSortRecursive unsortedList
+printfn "Posortowana lista rekurencyjnie: %A" sortedRecursive
