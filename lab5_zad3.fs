b7ff285934f5ff517abb4bf167dae34b2ab4879d
broniewskaK <115491327+broniewskaK@users.noreply.github.com>
2024-12-20 16:26:40 +0100

    Add files via upload

---
+++ lab5_zad3.fs
@@ -0,0 +1,17 @@
+﻿
+let rec permutations list =
+    match list with
+    | [] -> [[]] // jezeli lista pusta to zwróć listę pusta
+    | _ -> 
+        list
+        |> List.collect(fun x -> 
+                  permutations (List.filter ((<>) x ) list)
+                  |> List.map( fun perm -> x :: perm))
+
+
+let perm = permutations [1;2;3]
+
+
+printfn "permutacja dla listy [1;2;3]: "
+
+perm |> List.iter (fun item -> printfn "%A" item)
