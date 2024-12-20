47e7ab8e7f5014f2a63ffaf13d630f5770f6879c
broniewskaK <115491327+broniewskaK@users.noreply.github.com>
2024-12-20 16:30:45 +0100

    Add files via upload

---
+++ lab5_zad4wr.fs
@@ -0,0 +1,11 @@
+﻿// rekurencyjna Hanoi
+let rec hanoiRecursive n source target auxiliary =
+    if n = 1 then
+        printfn "Przenieś dysk z %s do %s" source target
+    else
+        hanoiRecursive (n - 1) source auxiliary target
+        printfn "Przenieś dysk z %s do %s" source target
+        hanoiRecursive (n - 1) auxiliary target source
+
+printfn "Rekurencyjne rozwiązanie problemu wież Hanoi dla 3 dysków:"
+hanoiRecursive 3 "A" "C" "B"
